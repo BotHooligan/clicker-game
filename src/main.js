210112cadc3d722705a19c041e14f0a92021ab98
Jayden N <jaydenn989@gmail.com>
2025-01-13 12:45:46 -0500

    Add files via upload
    Finished some clicker part and building part and stuff

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
-const tickRate = 1000 / 30;
+const tickRate = 1000 / 30; // 30 FPS
 
 let score = 0;
 
-let axe = new Building('Axe', 0.1, 15, 'buyAxe');
+let saw = new Building('Saw', 0.1, 1, 'buySaw')
+let axe = new Building('Axe', 0.25, 15, 'buyAxe');
+let chainsaw = new Building('Chainsaw', 1, 100, 'buyChainsaw');
 
 function incScore(){
+    score += saw.cps;
     score += axe.cps;
+    score += chainsaw.cps;
 }
 
 function scorePlusPlus(){
@@ -13,8 +17,15 @@ function scorePlusPlus(){
     
 }
 
+function updateButtons(){
+    axe.buttonState();
+    chainsaw.buttonState();
+    saw.buttonState();
+}
+
 function updatePage(){
     incScore();
+    updateButtons();
     document.getElementById("score").innerHTML = 
     Math.floor(score).toLocaleString();
 }
