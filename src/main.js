bbe86d04f394f1babf3c3ebaccaa9c91b5f74f49
Jayden N <jaydenn989@gmail.com>
2025-02-03 12:41:19 -0500

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,45 +1,71 @@
 const tickRate = 1000 / 30; // 30 FPS
 
-let clickStrength = 1;
-let score = 500;
+let clickStrength = 5000;
+let score = 0;
 
+let saw = new Building("Saw", 0.1, 15);
+let axe = new Building("Axe", 0.25, 100);
+let chainsaw = new Building("Chainsaw", 1, 1000);
+let dynamite = new Building("Dynamite", 2, 5000);
+let harvester = new Building("Harvester", 5, 20000);
+let hacker = new Building("Super Axe Hacker", 100, 100000);
 
+let sharpenSaw = new sharpen("Sharpen Saw", 100, saw);
+let sharpenAxe = new sharpen("Sharpen Axe", 500, axe);
+let sharpenChainsaw = new sharpen("Sharpen Chainsaw", 2000, chainsaw);
+let sharpenDynamite = new sharpen("More Dynamite", 15000, dynamite);
+let sharpenHarvester = new sharpen("More arms for Harvester", 50000, harvester);
+let sharpenHacker = new sharpen("Sharpen Super Axe Hacker", 1000000, hacker);
 
-let saw = new Building('Saw', 0.1, 15);
-let axe = new Building('Axe', 0.25, 100);
-let chainsaw = new Building('Chainsaw', 1, 1000);
+wps =
+  saw.cps + axe.cps + chainsaw.cps + dynamite.cps + harvester.cps + hacker.cps;
 
-let sharpenSaw = new sharpen('Sharpen Saw', 100, saw);
-
-let sharpenAxe = new sharpen('Sharpen Axe', 500, axe);
 //Update Score for each click
-function scorePlusPlus(){
-    score += clickStrength;
+function scorePlusPlus() {
+  score += clickStrength;
 }
 
-
-function incScore(){
-    score += saw.cps;
-    score += axe.cps;
-    score += chainsaw.cps;
+function incScore() {
+  score += saw.cps;
+  score += axe.cps;
+  score += chainsaw.cps;
+  score += dynamite.cps;
+  score += harvester.cps;
+  score += hacker.cps;
 }
 
-function updateButtons(){
-    saw.buttonState();
-    axe.buttonState();
-    chainsaw.buttonState();
-    sharpenSaw.buttonState();
-    sharpenAxe.buttonState();
-
+function updateButtons() {
+  saw.buttonState();
+  axe.buttonState();
+  chainsaw.buttonState();
+  dynamite.buttonState();
+  harvester.buttonState();
+  hacker.buttonState();
+  sharpenSaw.buttonState();
+  sharpenAxe.buttonState();
+  sharpenChainsaw.buttonState();
+  sharpenDynamite.buttonState();
+  sharpenHarvester.buttonState();
+  sharpenHacker.buttonState();
 }
 
-function updatePage(){
-    incScore();
-    updateButtons();
+function updatePage() {
+  incScore();
+  updateButtons();
+  wps =
+    ((saw.cps +
+      axe.cps +
+      chainsaw.cps +
+      dynamite.cps +
+      harvester.cps +
+      hacker.cps) *
+      1000) /
+    tickRate;
 
-    //update score display
-    document.getElementById("score").innerHTML = 
+  //update score display
+  document.getElementById("score").innerHTML =
     Math.floor(score).toLocaleString();
+  document.getElementById("wps").innerHTML = wps.toLocaleString();
 }
 
 setInterval(updatePage, tickRate);
